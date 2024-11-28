e558c159d092f950f955c03e3c56a4dfa6cfe553
Teoom <ararama199915@gmail.com>
2024-11-28 23:31:37 +0700

    add js-func: renderCard, initCard, deleteCard, getPopupElement, popupDisplay

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,70 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+initCards()
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+const addCardBtn = document.querySelector(".profile__add-button");
+const popupNewCard = document.querySelector('.popup_type_new-card');
 
-// @todo: Вывести карточки на страницу
+addCardBtn.addEventListener("click", () => {
+  popupDisplay(popupNewCard)
+})
+
+const popupClose = getPopupElement(popupNewCard, ".popup__close")
+popupClose.addEventListener("click", () => {
+  popupDisplay(popupNewCard);
+})
+
+const popupForm = getPopupElement(popupNewCard, ".popup__form")
+popupForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  cardList.append(renderCard({ name: evt.target[0].value, link: evt.target[1].value }))
+
+  evt.target[0].value = "";
+  evt.target[1].value = "";
+
+  popupDisplay(popupNewCard);
+
+})
+
+
+// Ренденр карточки
+function renderCard(card) {
+  const cardItem = cardTemplate.querySelector(".places__item").cloneNode(true);
+
+  // заместо const cardImage = cardItem.children[0]; const cardDeleteBtn = cardItem.children[1]... 
+  const [cardImage, cardDeleteBtn, cardDescription] = cardItem.children;
+
+  cardImage.src = card.link;
+  cardDescription.textContent = card.name;
+
+  cardDeleteBtn.addEventListener("click", () => {
+    deleteCard(cardDeleteBtn)
+  })
+
+  return cardItem;
+}
+
+// Удалене карточки
+function deleteCard(card) {
+  card.parentElement.remove()
+}
+
+// Иницилизация карточек
+function initCards() {
+  initialCards.forEach(card => {
+    cardList.append(renderCard(card));
+  })
+}
+
+// Получаем элемент определённого
+function getPopupElement(parentPopup, className) {
+  return parentPopup.querySelector(`${className}`)
+}
+
+// Закрытие/открытие попапа
+function popupDisplay(popup) {
+  popup.style.display = popup.style.display === "flex" ? "none" : "flex";
+}
